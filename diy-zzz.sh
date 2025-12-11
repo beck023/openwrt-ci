828f01c9f5c1056af65b89289293d3306c878ce2
beck023 <44996115+beck023@users.noreply.github.com>
2025-12-11 09:43:29 +0800

    Update diy-zzz.sh

--- diy-zzz.sh
+++ diy-zzz.sh
@@ -2,3 +2,12 @@
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.67.1/g' package/base-files/files/bin/config_generate
+
+# 修改本地时间格式
+sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+
+# 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
+
